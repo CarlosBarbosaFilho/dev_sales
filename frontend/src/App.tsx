e57135b001ddbb62d3ac1e7d8698ffdb24111e76
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2021-05-05 21:41:36 -0300

    create charts to dashboard - chart donut and bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
 import Navbar from 'components/nav_bar';
 import Footer from 'components/footer';
 import Table from 'components/tables';
+import BarChart from 'components/bar_chart';
+import DonutChart from 'components/donutChart';
+
 function App() {
   return (
     <>
-      <Navbar />
       <div className="container my-md-2">
-        <h2 className="text-primary">Finanças Acme</h2>
-        <Table/>
+        <Navbar />
+        <h2 className="text-primary">Vendas Acme</h2>
+        <hr />
+        <div className="row px-6">
+          <div className="col-sm-6">
+            <h2 className="text-center text-secondary">Meta do mês (%)</h2>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h2 className="text-center text-secondary">Todas as vendas realizadas</h2>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+        <Table />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/nav_bar/index.tsx
+++ frontend/src/components/nav_bar/index.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="container">
       <nav className="my-4 my-md-2 mr-md-3">
-        <img src={Financeiro} alt="DevSuperior" width="120" />
+        
       </nav>
     </div>
   );
